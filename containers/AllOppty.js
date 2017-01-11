4a9b53abbfdbdec88903fd0cedcdefe272ad688e
Zhijun Zhou <2050588049@qq.com>
2017-01-11 16:54:04 +0800

    all oppty list

--- containers/AllOppty.js
+++ containers/AllOppty.js
@@ -18,9 +18,39 @@ class AllOppty extends Component {
     }
 
     renderOppty(oppty) {
+        var bidMgrOrPem = null
+        var lpa = []
+
+        if(oppty.contact && oppty.contact.content) {
+            var content = oppty.contact.content
+            var leadRegion = oppty.leadRegion
+
+            if (content.primary.bidMgr != null) {
+                bidMgrOrPem = content.primary.bidMgr;
+            } else if (content[leadRegion].bidMgr != null) {
+                bidMgrOrPem = content[leadRegion].bidMgr;
+            } else if (content.primary.pursuitEngagementMgr != null) {
+                bidMgrOrPem = content.primary.pursuitEngagementMgr;
+            } else if (content[leadRegion].pursuitEngagementMgr != null) {
+                bidMgrOrPem = content[leadRegion].pursuitEngagementMgr;
+            } else {
+                bidMgrOrPem = null;
+            }
+
+            // lpa (Solution Consultant / Lead Practice Architect (LPA))
+            if (content.primary.solnConsultant != null && content.primary.solnConsultant.length > 0) {
+                lpa = content.primary.solnConsultant;
+            } else if (content[leadRegion].solnConsultant != null && content[leadRegion].solnConsultant.length > 0) {
+                lpa = content[leadRegion].solnConsultant;
+            } else {
+                lpa = [];
+            }
+        }
+
+
         return (
             <tr key={oppty.opptyId}>
-                <td>
+                <td className="text-nowrap">
                     {
                         oppty.isMine === true
                         ? <Link to={`/cs/${oppty.opptyId}?opptyName=${oppty.opptyName}`}>{oppty.opptyId}</Link>
@@ -28,7 +58,17 @@ class AllOppty extends Component {
                     }                    
                 </td>
                 <td>{oppty.opptyName}</td>
-                <td></td>
+                <td>{oppty.clientName}</td>
+                <td>
+                    {
+                        bidMgrOrPem ? <a href={'mailto:' + bidMgrOrPem.email}>{bidMgrOrPem.title}</a> : null
+                    }
+                </td>
+                <td>
+                    {
+                        lpa.map((role, index)=>(<div key={role.email + index}><a href={'mailto:' + role.email}>{role.title}</a></div>))
+                    }
+                </td>
                 <td></td>
             </tr>
         )
@@ -38,14 +78,15 @@ class AllOppty extends Component {
         const { allOppties } = this.props
         return (
             <div>
-                <b>All Opportunities</b>
-                <table width="100%">
-                    <thead>
+                <table width="100%" className="table table-striped table-bordered">
+                    <thead className="thead-inverse">
                         <tr>
-                            <td><b>Opportunity ID</b></td>
-                            <td><b>Opportunity Name</b></td>
-                            <td><b></b></td>
-                            <td><b></b></td>
+                            <td className="text-nowrap">Opportunity ID</td>
+                            <td className="text-nowrap">Opportunity Name</td>
+                            <td className="text-nowrap">Client Name</td>
+                            <td className="text-nowrap">Bid Manager / PEM</td>
+                            <td className="text-nowrap">Solution Consultant / Lead Practice Architect (LPA)</td>
+                            <td className="text-nowrap">Option Available</td>
                         </tr>
                     </thead>
                     <tbody>
